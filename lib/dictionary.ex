24b9657968b8b70b45c3feac3dd505c63d4e299f
Emilio <thecraftedgem@tuta.io>
2019-06-07 12:27:52 -0600

    WIP

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,7 +1,12 @@
 defmodule Dictionary do
 
-  def hello do
-    IO.puts("hello_world")
+  def word_list do
+    File.read!("assets/word.txt") |> String.split("\n")
+  end
+
+  def random_word  do
+    word_list()
+    |> Enum.random()
   end
 end
 
